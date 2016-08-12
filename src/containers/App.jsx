9833ea28533e33a3ed63628a440d12fdb6c3a02f
niconame <hazelluf@gmail.com>
2016-08-12 16:41:09 +0800

    class test

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,15 +1,16 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { addTodo, toogleTodo } from 'actions/TodoActions'
+import { addTodo, toggleTodo } from 'actions/TodoActions'
 import AddTodo from 'components/Todo/AddTodo'
 import TodoList from 'components/Todo/TodoList'
 
 class App extends Component {
   render() {
-    const { dispatch, todos } = this.props
+    const { dispatch, todos } = this.props;
     return (
       <div>
-        
+        <AddTodo onAddClick={text => dispatch(addTodo(text))}/>
+        <TodoList todos={todos} onTodoClick={index => dispatch(toggleTodo(index))}/>
       </div>
     )
   }
